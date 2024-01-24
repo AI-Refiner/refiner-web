247bd7159018ecd5789418b0bab795e4ef6ee6df
Alex Daro <alexdaro34@gmail.com>
2024-01-23 17:32:16 -0800

    Update Constants.js

--- src/Constants.js
+++ src/Constants.js
@@ -1,13 +1,9 @@
 export const API_HOST = "http://localhost:3000";
-// export const API_HOST =
-//   "https://zvxcccjrsf.execute-api.us-east-1.amazonaws.com";
 export const API_VERSION = "v1";
-// export const API_URL = `prod/${API_VERSION}`;
 export const API_URL = `/${API_VERSION}`;
 export const COGNITO_CLIENT_ID = "3j7kje3o9dkmnbn1mnp7bcsfd6";
 export const COGNITO_APP_URL =
   "https://refiner.auth.us-east-1.amazoncognito.com";
 export const COGNITO_REDIRECT_URI = `${API_HOST}/${API_VERSION}/auth/callback/`;
-// export const COGNITO_REDIRECT_URI = `${API_HOST}/prod/${API_VERSION}/auth/callback/`;
 export const REFINER_MEDIA_URL = "https://d3gtvjlg3g61pt.cloudfront.net/images";
 export const REFINER_GITHUB_URL = "https://github.com/AI-Refiner";
